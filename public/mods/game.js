a3a9a981a69a5c7932087323c317d24fd4c3a942
unknown <mast0645@fenton2162-08.ad.fredonia.edu>
2015-04-16 13:07:38 -0400

    New templates

---
+++ public/mods/game.js
@@ -0,0 +1,24 @@
+var Game = (function() {
+  var variable = {
+
+  };
+
+  // "Global" "Protected" variables
+
+
+
+  return {
+      functionName: function (argument) {
+        // body...
+      },
+      functionName: function () {
+        // body...
+      },
+      functionName: function () {
+        // body...
+      }
+  }
+})() ;
+
+
+Game.functionName()
---
+++ public/mods/waitingRoom.js
@@ -0,0 +1,44 @@
+
+
+//will control events (database/mods are figuring out)
+//server emits events like 
+	//How much time left until games starts. 
+
+//HTML Chat file (var io =)
+//function we call to emit an event like "connect", "Im here", "Im ready", 
+
+//global protected area 
+	//list event handlers
+		//IO.on game start
+		//IO.on update time 
+		//whatever they figure out
+		
+
+//scaffold object for them to fill out with code. 
+
+
+var watingRoom = (function() {
+  var variable = {
+
+  };
+
+  // "Global" "Protected" variables
+
+
+
+  return {
+      functionName: function (argument) {
+        // body...
+      },
+      functionName: function () {
+        // body...
+      },
+      functionName: function () {
+        // body...
+      }
+  }
+})() ;
+
+
+waitingRoom.functionName()
+
