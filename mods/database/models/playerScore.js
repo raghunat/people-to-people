4a010ba7d6f2e950c050fa71e21ee887576c39d7
FredEb <tour8647@fredonia.edu>
2015-04-16 13:16:25 -0400

    Update playerScore.js

--- mods/database/models/playerScore.js
+++ mods/database/models/playerScore.js
@@ -4,22 +4,25 @@ Author: mcdo7187@fredonia.edu
 Date: 4/6/2015
 Version: 0.0.5
 *******************************************************************************/
-
 var mongoose = require('mongoose');
+var cards = require('card');
+var user = require('user');
 
-/// Schema for player score -- How do we relate this to the user though? ask Raga.
+// Schema for player score -- How do we relate this to the user though? ask Raga
+
 var playerScoreSchema = mongoose.Schema({
-    playerScore: {
-		gameScore: int,
-		overallScore: int
-    }
+	playerScore: {
+		gameScore = [cards],
+		overallScore: Number,
+		cardId: cards.Types.ObjectId,
+		userId: user.Types.ObjectId
+	}
 });
 
 // Makes the model for the schema useful
 module.exports = mongoose.model('PlayerScore', playerScoreSchema);
-
 // Updating User's gameScore and overallScore based off user's email which is unique.
 db.score.update(
-   { email: "abc123@gmail.com" },
-   { $inc: { score: 1, overallScore: 1 } }
+{ email: "abc123@gmail.com" },
+{ $inc: { score: 1, overallScore: 1 } }
 )
