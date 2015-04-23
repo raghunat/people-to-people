daeba7f842387c295d9dafaf542089351cb043d0
NicoAlexander <reed1086@fredonia.edu>
2015-04-23 13:01:27 -0400

    Updated card database
    Updated card database page with card win count and card use count

--- mods/database/models/card.js
+++ mods/database/models/card.js
@@ -1,30 +1,32 @@
-/******************************************************************************
-card.js
-Author: mcdo7187@fredonia.edu
-Date: 4/6/2015
-Version: 0.0.5
-
-
-Cards (model)
-
->> picID
->> pic
->> label
-
-*******************************************************************************/
-
 var mongoose = require('mongoose');
 
 //Creating a schema for cards
 var cardSchema = mongoose.Schema({
-    card: {
+    Card: {
 		      cardPicture: String,
-		      cardLabel: Number,	
-		      cardWins: Number,   	//Number of wins with this card used
-		      timesPlayed: Number,      //Number of times the card was played in total
-	      	      userWinner: // user.Id    //User who won the round
+		      cardLabel: Number,
+			  cardWins: Number, //Number of times card wins
+			  timesPlayed: Number //Total number of times played
+			  //userWinner: user.Id //User who won the round
     }
 });
 
 // Makes the model for the schema useful
 module.exports = mongoose.model('Card', cardSchema);
+
+/*** Methods ***/
+
+// Count number of times card is chosen
+cardSchema.methods.chosen = function (cb) {
+	$inc: {chooseCount: 1}
+}
+
+// Counts number of times card has won
+cardSchema.methods.won = function (cb) {
+	$inc: {winCount: 1}
+}
+
+// Query cards, send them to module
+Card.find({}, function(err, cards){
+ 	
+});
