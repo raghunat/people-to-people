9c30e6f3c0c2d0e00fb0f6ec07c51212654aab2a
MKedas <msadek93@gmail.com>
2015-04-23 13:34:51 -0400

    Game Callback
    -Saves gameSession (date and two arrays for player and location)
    
    -checks for an error

--- mods/database/models/game.js
+++ mods/database/models/game.js
@@ -12,12 +12,19 @@ var mongoose = require('mongoose');
 
 var gameSchema = mongoose.Schema({
     gameSession: {
-        gameCreatedAt:  Date;
-        gameLocation: Array;
-        gamePlayers: Array ;
+        createdAt:  Date  ;
+        location:   Array ;
+        players:    Array ;
 
     }
 });
 
 // Makes the model for the schema useful
 module.exports = mongoose.model('game', gameSchema);
+
+//game players, game location, error
+gameSession.save(function(gameSession, err ){
+  if (err) return console.error(err);
+  gameSession.save();
+
+});
