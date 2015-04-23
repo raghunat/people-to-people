b0aef77ba160ba893b36abdcd5a0ec0096647b4c
Professor Raghunath <raghunat@fredonia.edu>
2015-04-23 09:57:05 -0400

    Adding comments

--- mods/test/waitingRoom_io_test.js
+++ mods/test/waitingRoom_io_test.js
@@ -5,10 +5,14 @@ describe('Testing Socket IO connection', function () {
   this.timeout(30000); //keep alive for 30 secs
   it('should connect to a running instance', function (done) {
     var client = io.connect('http://localhost:3000/waiting-room'); // Notice /Namespace
+
+    // When I connect
     client.on('connect', function (id) {
       console.log('Emmitting register event');
       client.emit('register', 'MyIdFromSessionOrCookie');
     });
+
+    // When the server says okay
     client.on('registered', function (response) {
       console.log('Recieving registered event');
       response.connected.should.be.true; //jshint ignore:line
