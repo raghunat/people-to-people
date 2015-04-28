e6b6771bc77e8ecc332fadb16de837c7fe589344
Adam Moncure <monty845@gmail.com>
2015-04-28 12:24:37 -0400

    Current progress on io integration into gameLayer

--- mods/gameLayer.js
+++ mods/gameLayer.js
@@ -1,69 +1,71 @@
-var playerLayer = require('./playerLayer.js');
-var gamesArray = playerLayer.makeGroups(peopleArray);
+var gameLayer = {
 
-var waitingRoom = [];
-var gamesArray;
-var cardsArray;
-
-
-io.on('enter waiting room', function(playerID, location){
-	//Pass player ID and location that the player got from geolocator
-	//Code to connect to database, get name of player, get score for player, get lastKing if we want to, add to data object
-});
-
-
-//When the database has returned the player info, add player to the game room player array,
-databasecallback.on('Database Returned Player Info' function(playerID, data){
-	if(data.valid){
-	waitingRoom.push(data);
-	//Emit to client to indicate the waiting room has been joined, emit to client time until next round of games
-	io.emit(data.playerID, "displayWaitingRoom" );
-	} else {
-	io.emit(PlayerID, "Error connecting to database");
-	}
-});
+//var Cards = require('./database/models/card.js');
 
 
 
-//io.on:If player leaves cleanly prior to game start, remove player from waiting room
-io.on('playerdisconnect', function(playerID){
-	var delIndex = waitingRoom.FindIndexOf(playerID);
-	waitingRoom.splice(delIndex, 1);
-});
 
+	prepareGames: function(waitingRoom){
+		console.log("starting to prepare game");
+		console.log(waitingRoom.sockets);
+		var playerLayer = require('./playerLayer.js');
+		var gamesArray = [];
+		//Create geolocation groups using the location data for all players in waiting room
+		//var regions = geoLocationFunction(watingRoom)
+		var regions = [];
+		regions[0] = waitingRoom.sockets; //Make it work till we do geolocation
+		console.log(regions);
+		//Call make groups for each geolaction group
+		for(var i = 0; i < regions.length; i++){
+			console.log("Creating group for region: " + i);
+			console.log(regions[i]);
+			var tempArray = playerLayer.makeGroups(regions[i]) //Temp array will be an array of 10 player games
+			console.log(tempArray);
+			for (var j = 0; j < tempArray.length; j++){
+				gamesArray.push(tempArray[j]);//We are looping through each array of 10 player games, and adding the individual games to the gamesArray
+			}
+		}
+		
 
+		//Get a set of cards
+		gameLayer.getNewCardsArray(gamesArray)
 
-function prepareGames(waitingRoom){
-	//Create geolocation groups using the location data for all players in waiting room
-	var regions = geoLocationFunction(watingRoom)
-	gamesArray = [];
-
-	//Call make groups for each geolaction group
-	for(var i = 0; i < regions.length; i++){
-		gamesArray.append(playerLayer.makeGroups(regions[i]));
-	}
+		console.log(gamesArray);
+	},
 	
-	//Get a set of cards
-	getNewCardsArray()
-};
+	getNewCardsArray: function(gamesArray){
+		/*Card.find({}, function(err, cards){
+			
+		}); */
+		var cardArray = [];
+		for(var i=0; i<100; i++){
+			cardArray.push(i);
+		}
+		gameLayer.newCardsArrayCallback(gamesArray, cardArray);
 
+	},
 
-newCardsArrayCallback(cardArray){
-	//Call assignCards for each geolocation group
-	playerLayer.assignCards(gamesArray, cardArray);
+	newCardsArrayCallback: function(gamesArray, cardArray){
+		//Call assignCards for each geolocation group
+		var playerLayer = require('./playerLayer.js');
+		playerLayer.assignCards(gamesArray, cardArray);
 
-	//Pass gamesArray to game starting function;
-	startGames(gamesArray);
+		//Pass gamesArray to game starting function;
+		//startGames(gamesArray);
+	}
+	/*
+	function startGames(gamesArray){
+		console.log(Game Started);
+			//io.emit: role of player
+			//io.emit: cards if pawn
+			//io.on: Card picked : remove all other cards from player object, add player object to a submitted object
+			//Wait for all player to pick or timeout has to occur
+			//io.emit: submitted object
+			//io.on: selection from king : Add a winner property to winner, increase score by 1
+			//io.emit: submitted object with winner set
+			//Function to report results to database
+			//Once players are done viewing results, they can send themselves back to waiting room by emmiting 'enter waiting room'	
+	}; */
 }
 
-function startGames(gamesArray){
-		//io.emit: role of player
-		//io.emit: cards if pawn
-		//io.on: Card picked : remove all other cards from player object, add player object to a submitted object
-		//Wait for all player to pick or timeout has to occur
-		//io.emit: submitted object
-		//io.on: selection from king : Add a winner property to winner, increase score by 1
-		//io.emit: submitted object with winner set
-		//Function to report results to database
-		//Once players are done viewing results, they can send themselves back to waiting room by emmiting 'enter waiting room'	
-};
+module.exports = gameLayer;
