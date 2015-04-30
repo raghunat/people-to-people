21ebee0275d732e2a2e92af5868aa8d16367c0d7
Adam Moncure <monty845@gmail.com>
2015-04-30 11:38:16 -0400

    Updates to gamelayer.js

--- mods/gameLayer.js
+++ mods/gameLayer.js
@@ -1,19 +1,23 @@
 var gameLayer = {
 
-//var Card = require('./database/models/card.js');
+//var Cards = require('./database/models/card.js');
 
 
 
 
 	prepareGames: function(waitingRoom){
 		console.log("starting to prepare game");
-		console.log(waitingRoom.sockets);
+		//console.log(waitingRoom.sockets);
 		var playerLayer = require('./playerLayer.js');
 		var gamesArray = [];
 		//Create geolocation groups using the location data for all players in waiting room
-		//var regions = geoLocationFunction(watingRoom)
+		//var regions = geoLocateGroups(watingRoom)
 		var regions = [];
-		regions[0] = waitingRoom.sockets; //Make it work till we do geolocation
+		regions.push([]);
+		for(var i = 0; i < waitingRoom.sockets.length; i++){
+			regions[0].push(waitingRoom.sockets[i]); //Make it work till we do geolocation, pushing each socket in the waiting room to region 0
+			//TODO we should have a regions array that is returned from the geoLocateGroups funciton
+		}
 		console.log(regions);
 		//Call make groups for each geolaction group
 		for(var i = 0; i < regions.length; i++){
@@ -25,18 +29,20 @@ var gameLayer = {
 				gamesArray.push(tempArray[j]);//We are looping through each array of 10 player games, and adding the individual games to the gamesArray
 			}
 		}
-		
 
 		//Get a set of cards
 		gameLayer.getNewCardsArray(gamesArray)
 
-		console.log(gamesArray);
+		//console.log(gamesArray);
 	},
 	
 	getNewCardsArray: function(gamesArray){
 		/*Card.find({}, function(err, cards){
-			
+			//TODO Get cards from database	
 		}); */
+
+
+		//Stand in for card array from find, generating array of 100 cards to randomly assign
 		var cardArray = [];
 		for(var i=0; i<100; i++){
 			cardArray.push(i);
@@ -50,12 +56,37 @@ var gameLayer = {
 		var playerLayer = require('./playerLayer.js');
 		playerLayer.assignCards(gamesArray, cardArray);
 
+		//Test emit of cards to players
+
+
 		//Pass gamesArray to game starting function;
-		//startGames(gamesArray);
-	}
-	/*
-	function startGames(gamesArray){
-		console.log(Game Started);
+		gameLayer.startGames(gamesArray);
+	},
+	
+	/********************************************
+	*
+	*	TODO:
+	*	Should probably move all the players from waiting room to either a game namespace,
+	*	and/or a series of game channels inside the name space. We don't actually need to, but
+	*	if we don't, we will end up running everything from waitingroom.io. Also, not exactly sure
+	*	how to code socket.on stuff, as the game array isn't a namespace, and the sockets aren't
+	*	direct members of it anyway
+	*
+	*
+	*********************************************/
+
+
+
+
+	startGames: function (gamesArray){
+		console.log("Game Started");
+		for(var i=0; i<gamesArray.length; i++){
+			for(var j=0; j<gamesArray[0].length; j++){
+				console.log(gamesArray[i][j].cards);
+				gamesArray[i][j].emit("role", gamesArray[i][j].role); //Emit role to each player
+				gamesArray[i][j].emit("cards", gamesArray[i][j].cards); //Emit each player's cards to that player
+			}
+		}
 			//io.emit: role of player
 			//io.emit: cards if pawn
 			//io.on: Card picked : remove all other cards from player object, add player object to a submitted object
@@ -65,7 +96,7 @@ var gameLayer = {
 			//io.emit: submitted object with winner set
 			//Function to report results to database
 			//Once players are done viewing results, they can send themselves back to waiting room by emmiting 'enter waiting room'	
-	}; */
+	}
 }
 
 module.exports = gameLayer;
